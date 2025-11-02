39cad9552291ca362ee0b4562dbb1c855af5465c
Shreyas Kamat <192124395+ShreyasNK2006@users.noreply.github.com>
2025-11-02 14:00:00 +0000

    Find the Difference

---
+++ problems/find-the-difference.cpp
@@ -0,0 +1,18 @@
+# Find the Difference
+Solved on 2025-10-16
+
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        int i=0;
+        while(i<s.length())
+            {
+                if(s[i]!=t[i])
+                    return t[i];
+                i++;
+            }
+        return t[i];
+    }
+};
