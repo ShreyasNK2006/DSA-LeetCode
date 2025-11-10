e22bf450d92e9baeff4797120590816fad56f10c
Shreyas Kamat <192124395+ShreyasNK2006@users.noreply.github.com>
2025-11-10 14:00:00 +0000

    Largest Rectangle in Histogram

---
+++ problems/largest-rectangle-in-histogram.cpp
@@ -0,0 +1,39 @@
+# Largest Rectangle in Histogram
+Solved on 2025-10-30
+
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        stack<int> st;
+        vector<int> leftsmall(heights.size()),rightsmall(heights.size());
+        for(int i=0;i<heights.size();i++)
+        {
+
+            while(!st.empty() && heights[st.top()]>=heights[i])
+            st.pop();
+            if(st.empty())
+            leftsmall[i]=0;
+            else
+                leftsmall[i]=st.top()+1;
+            st.push(i);
+        }
+        while(!st.empty())
+        st.pop();
+        for(int i=heights.size()-1;i>=0;i--)
+        {
+            while(!st.empty() && heights[st.top()]>=heights[i])
+            st.pop();
+            if(st.empty())
+            rightsmall[i] = heights.size()-1;
+            else 
+                rightsmall[i]=st.top()-1;
+            st.push(i);
+        }
+        int maxs=0;
+        for(int i=0;i<heights.size();i++)
+        {
+            maxs = max(maxs,(rightsmall[i]-leftsmall[i]+1)*heights[i]);
+        }
+        return maxs;
+    }
+};
