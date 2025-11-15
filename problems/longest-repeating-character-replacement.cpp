b293760cd3a5a6bf1b97251ad8cac4d8c2d0abef
Shreyas Kamat <192124395+ShreyasNK2006@users.noreply.github.com>
2025-11-15 14:00:00 +0000

    Longest Repeating Character Replacement

---
+++ problems/longest-repeating-character-replacement.cpp
@@ -0,0 +1,25 @@
+# Longest Repeating Character Replacement
+Solved on 2025-11-12
+
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int left=0,right=0;
+        int maxcount=0;
+        int minfreq=0;
+        vector<int> freq(26,0);
+        while(right<s.length())
+        {
+            freq[s[right]-'A']++;
+            minfreq =max(minfreq,freq[s[right]-'A']);
+            while(right-left+1-minfreq>k)
+            {
+                freq[s[left]-'A']--;
+                left++;
+            }
+            maxcount=max(maxcount,right-left+1);
+            right++;
+        }
+        return maxcount;
+    }
+};
