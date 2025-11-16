002572f8bec85146c995fbc811bdedb8c3307d70
Shreyas Kamat <192124395+ShreyasNK2006@users.noreply.github.com>
2025-11-16 14:00:00 +0000

    Partition Equal Subset Sum

---
+++ problems/partition-equal-subset-sum.cpp
@@ -0,0 +1,27 @@
+# Partition Equal Subset Sum
+Solved on 2025-11-12
+
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+        int totalsum=0;
+        int n=nums.size();
+        for(int i=0;i<n;i++)
+        {
+            totalsum+=nums[i];
+        }
+        if(totalsum%2!=0)
+        return false;
+        totalsum/=2;
+        vector<bool> dp(totalsum+1,false);
+        dp[0]=true;
+        for(int i=0;i<nums.size();i++)
+        {
+            for(int s=totalsum;s>=nums[i];s--)
+            {
+                dp[s] = dp[s]||dp[s-nums[i]];
+            }
+        }
+        return dp[totalsum];
+    }
+};
