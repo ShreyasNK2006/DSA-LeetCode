1338c752a0d676627aa377acc5f5d2ce2cfc33b4
Shreyas Kamat <192124395+ShreyasNK2006@users.noreply.github.com>
2025-11-18 14:00:00 +0000

    Coin Change

---
+++ problems/coin-change.cpp
@@ -0,0 +1,27 @@
+# Coin Change
+Solved on 2025-11-13
+
+class Solution {
+public:
+    void calcmincoin(vector<int>& coins, int amount,int &res)
+    {
+        vector<int> dp(amount+1,INT_MAX);
+        dp[0]=0;
+        for(int coin:coins)
+        {
+            for(int x =coin;x<=amount;x++)
+            {
+                if(dp[x-coin]!=INT_MAX)
+                dp[x]=min(dp[x-coin]+1,dp[x]);
+            }
+        }
+        res=(dp[amount]==INT_MAX?-1:dp[amount]);
+        return;
+        
+    }
+    int coinChange(vector<int>& coins, int amount) {
+        int res;
+        calcmincoin(coins,amount,res);
+        return res;
+    }
+};
