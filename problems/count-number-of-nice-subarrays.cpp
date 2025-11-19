9cbd9fd7a2a63578f90a4c0900bce3cd57c6393b
Shreyas Kamat <192124395+ShreyasNK2006@users.noreply.github.com>
2025-11-19 14:00:00 +0000

    Count Number of Nice Subarrays

--- problems/count-number-of-nice-subarrays.cpp
+++ problems/count-number-of-nice-subarrays.cpp
@@ -3,19 +3,32 @@ Solved on 2025-11-14
 
 class Solution {
 public:
+    int findsub(vector<int>& nums, int goal)
+    {
+        if(goal<0)
+        return 0;
+        int l =0,r=0,sum=0,cnt=0;
+        while(r<nums.size())
+        {
+            sum+=nums[r];
+                while(sum>goal)
+                {
+                    sum-=nums[l];
+                    l++;
+                }
+            
+            cnt+=(r-l+1);
+            r++;
+        }
+        return cnt;
+    }
     int numberOfSubarrays(vector<int>& nums, int k) {
-        int count =0;
-        int oddcount=0;
-        unordered_map<int,int> countpos;
-        countpos[0]=1;
         for(int i=0;i<nums.size();i++)
         {
-            if(nums[i]%2==1)
-            oddcount++;
-            if(countpos.find(oddcount-k)!=countpos.end())
-            count+=countpos[oddcount-k];
-            countpos[oddcount]++;
+            nums[i]=(nums[i]%2);
         }
-        return count;
+        int cnt =0;
+        cnt = (findsub(nums,k)-findsub(nums,k-1));
+        return cnt;
     }
 };
