e77b8cdba86d60b2109fe0155c89a0eb2435295f
Shreyas Kamat <192124395+ShreyasNK2006@users.noreply.github.com>
2026-04-07 02:00:00 +0000

    Add 2906: Construct Product Matrix

--- problems/2906-construct-product-matrix.cpp
+++ problems/2906-construct-product-matrix.cpp
@@ -2,8 +2,8 @@
  * Problem 2906: Construct Product Matrix
  * URL     : https://leetcode.com/problems/construct-product-matrix/
  * Solved  : 2026-03-24
- * Runtime : 132 ms
- * Memory  : 172.6 MB
+ * Runtime : 49 ms
+ * Memory  : 140.7 MB
  *
  * Explanation: (AI generation failed – check your GEMINI_API_KEY and quota.)
 */
@@ -13,27 +13,27 @@ class Solution {
 public:
     vector<vector<int>> constructProductMatrix(vector<vector<int>>& grid) {
         int m=grid.size(),n=grid[0].size();
-        vector<vector<long long>> lft(m+1,vector<long long>(n+1,1)),rht(m+1,vector<long long>(n+1,1));
+        vector<long long> prefix(m*n+1,1),suffix(m*n+1,1);
+        int x=0;
         for(int i=0;i<m;i++)
         {
-            
-                lft[i+1][0]=lft[i][n];
-                rht[m-i-1][n]=rht[m-i][0];
-
             for(int j=0;j<n;j++)
             {
-                lft[i+1][j+1]=lft[i+1][j];
-                lft[i+1][j+1]=(lft[i+1][j+1]*grid[i][j])%MOD;
-                rht[m-i-1][n-j-1]=rht[m-i-1][n-j];
-                rht[m-i-1][n-j-1]=(rht[m-i-1][n-j-1]*grid[m-i-1][n-j-1])%MOD;
+                prefix[x+1]=prefix[x];
+                suffix[m*n-x-1]=suffix[m*n-x];
+                x++;
+                prefix[x]=(prefix[x]*grid[i][j])%MOD;
+                suffix[m*n-x]=(suffix[m*n-x]*grid[m-i-1][n-1-j])%MOD;
             }
         }
+        x=0;
         vector<vector<int>> p(m,vector<int>(n));
         for(int i=0;i<m;i++)
         {
             for(int j=0;j<n;j++)
             {
-                p[i][j]=(lft[i+1][j]*rht[i][j+1])%MOD;
+                p[i][j]=(prefix[x]*suffix[x+1])%MOD;
+                x++;
             }
         }
         return p;
