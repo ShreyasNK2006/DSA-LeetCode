344bb3426539918e7f0c3aede7032545221814b0
Shreyas Kamat <192124395+ShreyasNK2006@users.noreply.github.com>
2026-04-08 02:00:00 +0000

    Add 2573: Find the String with LCP

---
+++ problems/2573-find-the-string-with-lcp.cpp
@@ -0,0 +1,55 @@
+/*
+ * Problem 2573: Find the String with LCP
+ * URL     : https://leetcode.com/problems/find-the-string-with-lcp/
+ * Solved  : 2026-03-28
+ * Runtime : 11 ms
+ * Memory  : 75.5 MB
+ *
+ * Explanation: (AI generation failed – check your GEMINI_API_KEY and quota.)
+*/
+
+class Solution {
+public:
+    string findTheString(vector<vector<int>>& lcp) {
+        int n = lcp.size();
+        string word(n, '\0');
+        char current = 'a';
+
+        for (int i = 0; i < n; i++) {
+            if (word[i] == '\0') {
+                if (current > 'z') {
+                    return "";
+                }
+                word[i] = current;
+                for (int j = i + 1; j < n; j++) {
+                    if (lcp[i][j] > 0) {
+                        word[j] = word[i];
+                    }
+                }
+                current++;
+            }
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                if (word[i] != word[j]) {
+                    if (lcp[i][j]) {
+                        return "";
+                    }
+                } else {
+                    if (i == n - 1 || j == n - 1) {
+                        if (lcp[i][j] != 1) {
+                            return "";
+                        }
+                    } else {
+                        if (lcp[i][j] != lcp[i + 1][j + 1] + 1) {
+                            return "";
+                        }
+                    }
+                }
+            }
+        }
+
+        return word;
+    }
+};
