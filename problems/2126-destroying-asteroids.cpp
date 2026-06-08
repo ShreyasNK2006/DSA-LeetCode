b278ec611fe541cfb0fe241e984dcb0336d1416b
Shreyas Kamat <192124395+ShreyasNK2006@users.noreply.github.com>
2026-06-08 02:00:00 +0000

    Add 2126: Destroying Asteroids

---
+++ problems/2126-destroying-asteroids.cpp
@@ -0,0 +1,38 @@
+/*
+ * Problem 2126: Destroying Asteroids
+ * URL     : https://leetcode.com/problems/destroying-asteroids/
+ * Solved  : 2026-05-31
+ * Runtime : 207 ms
+ * Memory  : 137.9 MB
+ *
+ * Explanation: (AI generation failed – check your GEMINI_API_KEY and quota.)
+*/
+
+class Solution {
+public:
+    bool asteroidsDestroyed(int mass, vector<int>& asteroids) {
+        map<long long,int> mp;
+        for(int i=0;i<asteroids.size();i++)
+        {
+            mp[asteroids[i]]++;
+        }
+        int c=0;
+        long long m=mass;
+        while(c!=asteroids.size())
+        {
+            auto it = mp.upper_bound(m);
+            if(it==mp.begin())
+            return false;
+            it--;
+            m+=it->first;
+            mp[it->first]--;
+            if(mp[it->first]==0)
+            mp.erase(it->first);
+            c++;
+        }
+        if(!mp.size())
+        return true;
+        else
+        return false;
+    }
+};
