0f1f6c08bfea82453adc83dcecea78f70d45990e
Shreyas Kamat <192124395+ShreyasNK2006@users.noreply.github.com>
2026-08-22 02:00:00 +0000

    Add 3016: Minimum Number of Pushes to Type Word II

---
+++ problems/3016-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -0,0 +1,37 @@
+/*
+ * Problem 3016: Minimum Number of Pushes to Type Word II
+ * URL     : https://leetcode.com/problems/minimum-number-of-pushes-to-type-word-ii/
+ * Solved  : 2026-07-31
+ * Runtime : 12 ms
+ * Memory  : 28.6 MB
+ *
+ * Explanation: (AI generation failed – check your GEMINI_API_KEY and quota.)
+*/
+
+class Solution {
+public:
+    int minimumPushes(string word) {
+        int c=0;
+        vector<int> f(26,0);
+        for(char c:word)
+        {
+            f[c-'a']++;
+        }
+        vector<int> sf;
+        for(int i=0;i<26;i++)
+        {
+            if(f[i]>0)
+            sf.push_back(f[i]);
+        }
+        sort(sf.rbegin(),sf.rend());
+        int k=0,factor=1;
+        for(auto x:sf)
+        {
+            c=c+factor*x;
+            k++;
+            if(k%8==0)
+            factor++;
+        }
+        return c;
+    }
+};
