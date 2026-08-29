8bdfe82a5355098135bde41bb2d6aec71b261fab
Shreyas Kamat <192124395+ShreyasNK2006@users.noreply.github.com>
2026-08-29 02:00:00 +0000

    Add 3090: Maximum Length Substring With Two Occurrences

--- problems/3090-maximum-length-substring-with-two-occurrences.cpp
+++ problems/3090-maximum-length-substring-with-two-occurrences.cpp
@@ -2,8 +2,8 @@
  * Problem 3090: Maximum Length Substring With Two Occurrences
  * URL     : https://leetcode.com/problems/maximum-length-substring-with-two-occurrences/
  * Solved  : 2026-08-14
- * Runtime : 14 ms
- * Memory  : 26.4 MB
+ * Runtime : 0 ms
+ * Memory  : 9 MB
  *
  * Explanation: (AI generation failed – check your GEMINI_API_KEY and quota.)
 */
@@ -12,23 +12,23 @@ class Solution {
 public:
     int maximumLengthSubstring(string s) {
         int maxlen=0;
-        vector<queue<int>> freq(26);
+        vector<int> freq(26,0);
         int l=-1;
         for(int i=0;i<s.size();i++)
         {
             int c = s[i]-'a';
-            if(freq[c].size()==2)
+            if(freq[c]==2)
             {
-                for(int j=l+1;j<freq[c].front();j++)
+                for(int j=l+1;(s[j]-'a')!=c;j++)
                 {
-                    freq[s[j]-'a'].pop();
+                    freq[s[j]-'a']--;
+                    l=j;
                 }
-                l=freq[c].front();
-                freq[c].pop();
-                
+                l++;
+                freq[c]--;
             }
             maxlen=max(maxlen,i-l);
-            freq[c].push(i);
+            freq[c]++;
         }
         return maxlen;
     }
