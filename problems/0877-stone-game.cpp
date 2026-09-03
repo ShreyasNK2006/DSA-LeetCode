f92eebfda3f89002ad16f0fec48bb8c046d0e3b4
Shreyas Kamat <192124395+ShreyasNK2006@users.noreply.github.com>
2026-09-03 02:00:00 +0000

    Add 0877: Stone Game

---
+++ problems/0877-stone-game.cpp
@@ -0,0 +1,16 @@
+/*
+ * Problem 0877: Stone Game
+ * URL     : https://leetcode.com/problems/stone-game/
+ * Solved  : 2026-08-02
+ * Runtime : 0 ms
+ * Memory  : 10.5 MB
+ *
+ * Explanation: (AI generation failed – check your GEMINI_API_KEY and quota.)
+*/
+
+class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+        return true;
+    }
+};
