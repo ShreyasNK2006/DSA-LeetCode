b61478ac14cad4233aca4ec9301394182a81e1bb
Shreyas Kamat <192124395+ShreyasNK2006@users.noreply.github.com>
2026-09-25 02:00:00 +0000

    Add 3875: Construct Uniform Parity Array I

---
+++ problems/3875-construct-uniform-parity-array-i.cpp
@@ -0,0 +1,16 @@
+/*
+ * Problem 3875: Construct Uniform Parity Array I
+ * URL     : https://leetcode.com/problems/construct-uniform-parity-array-i/
+ * Solved  : 2026-09-02
+ * Runtime : 0 ms
+ * Memory  : 30.3 MB
+ *
+ * Explanation: (AI generation failed – check your GEMINI_API_KEY and quota.)
+*/
+
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+            return true;
+    }
+};
